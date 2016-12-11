af0da17696880c931c392b186dbe167acceb6d5c
skyboong <stsboongkee@gmail.com>
2016-12-12 00:30:08 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+# Date : 2016.12.11
+# BY   : B. K. Choi
+
+from distutils.core import setup 
+
+setup( name        = 'Text Analysis',      
+       version     = '1.0',       
+       description = 'Text Analysis Tools',     
+       author      = 'BK Choi',       
+       author_email= 'stsboongkee@gmail.com',   
+       py_modules  = ['textanalysis.py'],   
+       packages    = ['']
+       url         = "https://github.com/skyboong/text.git"
+       )
